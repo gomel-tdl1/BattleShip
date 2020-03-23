f9717c862a1bd4325503ac47de2b4f74b8752ce5
Ilya Taldykin <rock_cell@mail.ru>
2020-03-23 22:31:19 +0300

    DOM, HTML, CSS complete without JS version

---
+++ Battle/game/battleship.js
@@ -0,0 +1,17 @@
+function seaShip() {
+    let sea = [
+        ['00', 10, 20, 30, 40, 50, 60],
+        ['01']
+    ];
+
+}
+
+function shout() {
+    let loc = document.search.location;
+
+
+}
+
+function hit(hit) {
+
+}
